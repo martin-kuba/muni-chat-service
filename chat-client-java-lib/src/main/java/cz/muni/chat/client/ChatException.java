22a3567ca25258fc640a27530125ac4e0e23d363
Martin Kuba <makub@ics.muni.cz>
2022-12-23 13:48:33 +0100

    made client compatible with Spring Boot 3

---
+++ chat-client-java-lib/src/main/java/cz/muni/chat/client/ChatException.java
@@ -0,0 +1,72 @@
+package cz.muni.chat.client;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cz.muni.chat.client.invoker.ApiException;
+import cz.muni.chat.client.model.ErrorMessage;
+
+import java.io.IOException;
+import java.time.OffsetDateTime;
+
+/**
+ * Exception created from ErrorMessage sent from ChatService for non-200 HTTP status codes.
+ */
+public class ChatException extends Exception {
+
+    public ChatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    private OffsetDateTime timestamp;
+
+    private Integer httpStatus;
+
+    private String httpStatusName;
+
+    private String path;
+
+    private static ObjectMapper objectMapper =
+            new ObjectMapper()
+                    .findAndRegisterModules()
+                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+    public static ChatException from(ApiException ex) {
+        try {
+            ErrorMessage em = objectMapper.readValue(ex.getResponseBody(), ErrorMessage.class);
+            ChatException che = new ChatException(em.getMessage(), ex);
+            che.timestamp = em.getTimestamp();
+            che.httpStatus = em.getStatus();
+            che.httpStatusName = em.getError();
+            che.path = em.getPath();
+            return che;
+        } catch (IOException ioe) {
+            return new ChatException("cannot parse remote Exception", ex);
+        }
+    }
+
+    public OffsetDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public Integer getHttpStatus() {
+        return httpStatus;
+    }
+
+    public String getHttpStatusName() {
+        return httpStatusName;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    @Override
+    public String toString() {
+        return "ChatException: " +
+                getMessage() + '\'' +
+                ", httpStatus=" + httpStatus + " (" + httpStatusName + ')' +
+                ", path='" + path + '\'' +
+                ", timestamp=" + timestamp
+                ;
+    }
+}
--- chat-client-java/src/main/java/cz/muni/chat/client/Main.java
+++ chat-client-java/src/main/java/cz/muni/chat/client/Main.java
@@ -1,34 +1,45 @@
 package cz.muni.chat.client;
 
 import cz.muni.chat.client.invoker.ApiClient;
+import cz.muni.chat.client.invoker.ApiException;
 import cz.muni.chat.client.model.BackgroundColorEnum;
 import cz.muni.chat.client.model.ChatMessage;
 import cz.muni.chat.client.model.NewChatMessageRequest;
-import org.springframework.web.client.RestClientException;
 
 import java.time.ZoneId;
+import java.util.Random;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         System.out.println("starting");
         ChatApi chat = new ChatApi(new ApiClient());
 
+        // list all messages
+        for (ChatMessage chatMessage : chat.getAllMessages()) {
+            System.out.println("chatMessage = " + chatMessage);
+        }
+
+        // create a new message
+        BackgroundColorEnum[] colors = BackgroundColorEnum.values();
+        BackgroundColorEnum bg = colors[new Random().nextInt(colors.length)];
+        ChatMessage message = chat.createMessage(
+                new NewChatMessageRequest()
+                        .text("Hello!")
+                        .textColor("black")
+                        .backgroundColor(bg),
+                "me",
+                "UltraChat 1.0");
+
+        System.out.println("new message = " + message);
+        System.out.println("timestamp: " + message.getTimestamp().atZoneSameInstant(ZoneId.systemDefault()));
+
+        // deliberately make a wrong call to show catching an exception
         try {
-            for (ChatMessage chatMessage : chat.getAllMessages()) {
-                System.out.println("chatMessage = " + chatMessage);
-            }
-
-            ChatMessage message = chat.createMessage(
-                    new NewChatMessageRequest().text("Hello!").textColor("black").backgroundColor(BackgroundColorEnum.AQUAMARINE),
-                    "me",
-                    null);
-            System.out.println("new message = " + message);
-            System.out.println("timestamp: "+ message.getTimestamp().atZoneSameInstant(ZoneId.systemDefault()));
-
-        } catch (RestClientException ex) {
-            ex.printStackTrace();
+            chat.getMessage("1");
+        } catch (ApiException ex) {
+            ChatException.from(ex).printStackTrace();
         }
 
     }
