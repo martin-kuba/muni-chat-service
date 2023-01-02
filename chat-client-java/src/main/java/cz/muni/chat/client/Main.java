d2f0616fe8cb3cdb88190a932557dffa923064bb
Martin Kuba <makub@ics.muni.cz>
2023-01-02 16:38:21 +0100

    changed client to spring boot app

--- chat-client-java/src/main/java/cz/muni/chat/client/Main.java
+++ chat-client-java/src/main/java/cz/muni/chat/client/Main.java
@@ -5,22 +5,35 @@ import cz.muni.chat.client.invoker.ApiException;
 import cz.muni.chat.client.model.BackgroundColorEnum;
 import cz.muni.chat.client.model.ChatMessage;
 import cz.muni.chat.client.model.NewChatMessageRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Random;
 
 import static cz.muni.chat.client.model.NewChatMessageRequest.TextColorEnum;
 
-public class Main {
+@SpringBootApplication
+public class Main implements CommandLineRunner {
 
-    public static void main(String[] args) throws Exception {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-        System.out.println("starting");
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("starting");
         ChatApi chat = new ChatApi(new ApiClient());
 
         // list all messages
         for (ChatMessage chatMessage : chat.getAllMessages()) {
-            System.out.println("chatMessage = " + chatMessage);
+            log.info("message: {}", chatMessage);
         }
 
         // create a new message
@@ -34,14 +47,15 @@ public class Main {
                 "me",
                 "UltraChat 1.0");
 
-        System.out.println("new message = " + message);
-        System.out.println("timestamp: " + message.getTimestamp().atZoneSameInstant(ZoneId.systemDefault()));
+        log.info("new message = {}", message);
+        ZonedDateTime timestamp = message.getTimestamp().atZoneSameInstant(ZoneId.systemDefault());
+        log.info("timestamp: {}", timestamp);
 
         // deliberately make a wrong call to show catching an exception
         try {
             chat.getMessage("1");
         } catch (ApiException ex) {
-            ChatException.from(ex).printStackTrace();
+            log.info("expected exception", ChatException.from(ex));
         }
 
     }
