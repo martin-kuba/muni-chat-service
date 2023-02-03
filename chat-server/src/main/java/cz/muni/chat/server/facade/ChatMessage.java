2c841966b0eed30f41a23641281fe0331f17f242
Martin Kuba <makub@ics.muni.cz>
2023-02-03 10:11:46 +0100

    wrapped text in in ChatMessage

--- chat-server/src/main/java/cz/muni/chat/server/facade/ChatMessage.java
+++ chat-server/src/main/java/cz/muni/chat/server/facade/ChatMessage.java
@@ -7,18 +7,24 @@ import jakarta.validation.constraints.NotBlank;
 import java.time.ZonedDateTime;
 
 /**
- * DTO for a chat message. Data Transfer Object that is stable for API, independent of internal StoredMessage class.
+ * DTO for a chat message. Data Transfer Object that is stable for API,
+ * independent of internal StoredMessage class.
  *
  * @see cz.muni.chat.server.service.StoredMessage
  */
 @Schema(title = "chat message",
         description = "represents a message in a chat"
 )
-public record ChatMessage(@NotBlank @Schema(description = "uuid", example = "123e4567-e89b-12d3-a456-426614174000")
+public record ChatMessage(@NotBlank
+                          @Schema(description = "uuid", example = "123e4567-e89b-12d3-a456-426614174000")
                           String id,
-                          @NotBlank @Schema(type = "string", format = "date-time", description = "time of creation", example = "2022-12-22T12:04:04.493908908+01:00")
+                          @NotBlank
+                          @Schema(type = "string", format = "date-time",
+                                  description = "time of creation",
+                                  example = "2022-12-22T12:04:04.493908908+01:00")
                           ZonedDateTime timestamp,
-                          @NotBlank @Schema(description = "text of message", example = "Hello! \uD83D\uDE00")
+                          @NotBlank
+                          @Schema(description = "text of message", example = "Hello! \uD83D\uDE00")
                           String text,
                           @Schema(description = "author of message", example = "John")
                           String author,
@@ -27,6 +33,7 @@ public record ChatMessage(@NotBlank @Schema(description = "uuid", example = "123
                           BackgroundColor backgroundColor) {
 
     public static ChatMessage fromStoredMessage(StoredMessage m) {
-        return new ChatMessage(m.id(), m.timestamp(), m.text(), m.author(), m.textColor(), BackgroundColor.fromValue(m.backgroundColor()));
+        return new ChatMessage(m.id(), m.timestamp(), m.text(),
+                m.author(), m.textColor(), BackgroundColor.fromValue(m.backgroundColor()));
     }
 }
